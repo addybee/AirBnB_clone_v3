22cdc3f8fdd35c3ad8dc7725ace59bb04014e017
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-05-26 09:15:16 +0100

    custom 404 response and stat

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,3 +10,4 @@ from api.v1.views.index import *
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 app_views.add_url_rule("/status", view_func=get_status, methods=['GET'])
+app_views.add_url_rule("/stats", view_func=get_index, methods=['GET'])
