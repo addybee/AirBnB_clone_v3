d9001f9862a07c172a87d56a319ea6166f5bd94e
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-05-26 20:26:28 +0100

    revert some changes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,7 +6,7 @@ This module initializes a Flask Blueprint for API version 1 views.
 
 
 from flask import Blueprint
-from api.v1.views.index import *
-
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+from api.v1.views.index import *
